8f72515c9ae2e96c06b63094a6026ef981a521ad
equators113 <equators113@gmail.com>
2018-09-15 14:49:25 +0900

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
+    #djangogirls
+    '''sdjlakjdlaksjdlaksjdlaksdjlajdlakdjlajdlaksjdlaksdjalskdjald
+    jaskdlajdkjalsjdaldjalkdjasl'''
+
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+hi()
+
+for i in range(1, 6):
+    print(i)
