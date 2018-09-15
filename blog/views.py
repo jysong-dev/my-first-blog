d9e721336ab836f848e9c1047f90be44ec3f1415
equators113 <equators113@gmail.com>
2018-09-15 17:14:38 +0900

    CSS file added

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # publish date가 없으면 목록에 표시되지 않음
     return render(request, 'blog/post_list.html', {'posts':posts})
